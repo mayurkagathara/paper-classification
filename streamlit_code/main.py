5937c12e66cee014586127a52a37f7ea9cc8d4bd
mayurkagathara <mayur.kagathara2508@gmail.com>
2021-09-05 22:44:41 +0530

    streamlit_code_file in same folder

---
+++ streamlit_code/main.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  5 13:41:21 2021
+
+@author: 1353271
+"""
+
+import pandas as pd
+from prediction_module import predict_topics, predict_topics_list, predict_single
+import streamlit as st
+labels_ = ['Computer Science', 'Physics', 'Mathematics','Statistics', 'Quantitative Biology', 'Quantitative Finance']
+
+st.set_page_config(page_title="whitepaper_classifier",
+                   page_icon="📝",
+                   layout="wide")
+
+# predict_topics(raw_testing_data)
+# predict_topics_list(raw_testing_data)
+# row = 100
+# predict_single(raw_testing_data.loc[row,'TITLE'],raw_testing_data.loc[row,'ABSTRACT'])
+
+st.title('Whitepaper classifier')
+
+st.header('Single paper classification')
+st.subheader('Enter title and abstract to get topics')
+# st.button('Get Topics')
+
+with st.form("form_single_paper"):
+    title = st.text_input('Title', value='')
+    abstract = st.text_input('Abstract', value='')
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Get Topics")
+    if submitted:
+        topics = predict_single(title, abstract)
+        st.write('This whitepaper belongs to ',topics)
+
+
+st.header('Bulk paper classification')
+st.subheader('upload csv files to get predictions')
+# st.button('Get Topics')
+
+@st.cache
+def convert_df(df):
+    # Cache the conversion to prevent computation on every rerun
+    return df.to_csv(index=False).encode('utf-8')
+
+@st.cache
+def get_predictions(raw_data_file, topics=False):
+    if topics:
+        predictions = predict_topics_list(raw_testing_file)
+    else:
+        predictions = predict_topics(raw_testing_file)
+    return predictions
+
+
+
+with st.form("form_csv_file"):
+    input_file = st.file_uploader("upload comma separated file having title and abstract columns. Column names: TITLE, ABSTRACT", accept_multiple_files=False)
+    get_topics_list = st.radio('Do you want to get topics list in the prediction file?', ('yes', 'no'))
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Get Prediction file")
+    if submitted:
+        try:
+            raw_testing_file = pd.read_csv(input_file)
+            output_df = raw_testing_file.copy()
+            if get_topics_list=='yes':
+                predictions = get_predictions(raw_testing_file, topics=True)
+                labels_list = labels_ + ['topics']
+            else:
+                predictions = get_predictions(raw_testing_file, topics=False)
+                labels_list = labels_
+                
+            output_df.loc[:,labels_list] = predictions
+            st.dataframe(output_df)
+            success = True
+        except:
+            st.warning("Something is wrong with the file. Try again...")
+            success = False
+        
+if submitted and success:
+    csv = convert_df(output_df)
+    st.download_button(label="Press to Download",
+                       data=csv,
+                       file_name='predictions_file.csv',
+                       mime='text/csv')
---
+++ streamlit_code/prediction_module.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  5 13:25:14 2021
+
+This module loads TFIDF vectorizer and trained model.
+It predicts the topics given raw query data.
+
+@author: 1353271 Mayur Kagathara
+"""
+
+import dill as pickle
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from nltk.corpus import stopwords
+
+# Global variables/objects
+try:
+    stop_words = stopwords.words('english')
+except:
+    nltk.download('stopwords')
+    stop_words = stopwords.words('english')
+
+labels_ = ['Computer Science', 'Physics', 'Mathematics','Statistics', 'Quantitative Biology', 'Quantitative Finance']
+voting_model_file = 'voting_model.sav'
+title_vectorizer = pickle.load(open('title_tfidf_vectorizer_6000f.sav','rb'))
+abstarct_vectorizer = pickle.load(open('abstract_tfidf_vectorizer_6000f.sav','rb'))
+with open(voting_model_file, 'rb') as fread:
+    clf_model_voting = pickle.load(fread)
+
+
+def clean_transform_title_abstract(dataframe, title_vectorizer, abstarct_vectorizer):
+    '''
+    Takes dataframe, title TFIDF vectorizer and abstract vectorizer as input.
+    Returns tfidf vectorized title and abstract columns.
+    This function only applies transformation.
+    '''
+    def text_clean(text):
+        snowball_stemmer = SnowballStemmer("english")
+        pattern = r'[^a-zA-Z0-9\s]'
+        cleaned_sent = re.sub(pattern,'',text)
+        word_tokens = cleaned_sent.split()
+        word_tokens_stemmed = [snowball_stemmer.stem(w) for w in word_tokens if w not in stop_words]
+        return ' '.join(word_tokens_stemmed)
+    
+    title_col = dataframe.TITLE
+    cleaned_abstarct_col = dataframe.ABSTRACT.map(text_clean)
+    
+    title_col_tfidf = title_vectorizer.transform(title_col)
+    abstract_col_tfidf = abstarct_vectorizer.transform(cleaned_abstarct_col)
+    return title_col_tfidf, abstract_col_tfidf
+
+def predict_topics(raw_data):
+    title_col_vec_test, abstract_col_vec_test = clean_transform_title_abstract(raw_data, 
+                                                                 title_vectorizer, 
+                                                                 abstarct_vectorizer)
+    # print(title_col_vec_test.shape)
+    # print(abstract_col_vec_test.shape)
+    
+    title_features_test = pd.DataFrame(title_col_vec_test.todense(), columns=title_vectorizer.vocabulary_)
+    abstract_features_test = pd.DataFrame(abstract_col_vec_test.todense(), columns=abstarct_vectorizer.vocabulary_)
+    X_test = pd.concat([title_features_test, abstract_features_test], axis=1)
+    
+    y_pred = clf_model_voting.predict(X_test)
+    predictions = pd.DataFrame(y_pred, columns=labels_)
+    return predictions 
+
+def predict_single(title, abstract):
+    raw_data = pd.DataFrame([[title,abstract]], columns=['TITLE','ABSTRACT'])
+    predictions_list = predict_topics_list(raw_data)
+    return predictions_list.topics[0]
+
+def convert_topics_list(predictions_dataframe):
+    predictions_dataframe['topics'] = None
+    label_array = np.array(labels_)
+    for row in predictions_dataframe.T:
+        label_out = np.array(predictions_dataframe.loc[row,labels_])
+        predictions_dataframe.loc[row, 'topics'] = ', '.join(label_array[label_out.astype(bool)])
+    return predictions_dataframe
+
+def predict_topics_list(raw_data):
+    predictions_dataframe = predict_topics(raw_data)
+    return convert_topics_list(predictions_dataframe)
+    
+if __name__ == '__main__':
+    pass
